88fe5fb94e18ffb9165cda5515b61f813bede82d
KhanhMeowMeow <136141884+KhanhMeowMeow@users.noreply.github.com>
2023-11-16 20:14:17 +0700

    Delete Quanly_RapPhim directory

--- Quanly_RapPhim/main/main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-public class main {
-    public static void main(String[] args) {
-        new mainFrame().setVisible(true);
-        System.out.println("Đã hiện màn hình");
-    }
-
-}
