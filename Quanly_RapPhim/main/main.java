f211bb65231c4e23cd879b6145ca9d4aff5b902f
khanh <nguyenkhanhahihivl@gmail.con>
2023-11-16 18:13:26 +0700

    concặc

---
+++ Quanly_RapPhim/main/main.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+public class main {
+    public static void main(String[] args) {
+        new mainFrame().setVisible(true);
+        System.out.println("Đã hiện màn hình");
+    }
+
+}
