69c268168ade816b76888ced69e435fb2b8db345
KhanhMeowMeow <136141884+KhanhMeowMeow@users.noreply.github.com>
2023-11-30 08:52:58 +0700

    khanhcommit

--- code.cpp
+++ code.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
 int main(){
-<<<<<<< HEAD
-<<<<<<< HEAD
      count << "Chào cái quần què";
 }
-=======
-     count << "Chào mấy cưng";
-=======
-     count << "Chào anh Phát";
->>>>>>> fa3990c27e9bf59d7cf86d23654969de71c0c0ed
-}
->>>>>>> 0f4e0ec341eef60737a6f5b1ce6490ad7f9a95d7
+
